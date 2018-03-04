864b232f8632bfe44e8c5f7c92f5a3026c0316b9
Jim Kincaid <jamesedwardkincaid@gmail.com>
2018-03-03 19:30:56 -0500

    Initial commit into GIT

---
+++ Assets/AdsScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsScript : MonoBehaviour {
+
+	void Start () {
+
+	}
+	
+	void Update () {
+			ShowAd();
+	}
+
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+		}
+	}
+}
---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Gender gender = Gender.Male;
+		Analytics.SetUserGender(gender);
+
+		int birthYear = 1982;
+		Analytics.SetUserBirthYear(birthYear);
+		LevelUp();
+	}
+	
+	void LevelUp()
+	{
+		Analytics.Transaction("leveUp", 1, "USD", null, null);
+
+		Analytics.CustomEvent("levelUp", new Dictionary<string, object>
+		{
+			{ "newLevel", 2 },
+			{ "trigger", "purchase" },
+		});
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
